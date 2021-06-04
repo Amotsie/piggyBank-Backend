0e5ed30d5019a555f66ad4940a190b2fe5665730
Andreas Motsie <motsie.at@gmail.com>
2021-06-04 04:54:28 +0300

    Mongo URI fix

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const connect = await mongoose
-    .connect(process.env.MONGO_URI, {
-	  autoIndex:false
-    })
+    .connect(process.env.MONGO_URI, {})
     .then(() => console.log("connected to MongoDB 😎"))
     .catch((err) => console.log(err.message));
 };
 module.exports = connectDB;
+
+// autoIndex: false,
